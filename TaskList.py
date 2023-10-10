ac4e06ee2fb5d4d98df2a42213444b3029ddc1d2
Jaime Montealegre <jaime.montealegre@jellysmack.com>
2023-10-10 16:29:07 +0200

    Adding todo list

---
+++ TaskList.py
@@ -0,0 +1,25 @@
+class TaskLikst():
+
+    # TODO: Create Task List
+    def __init__(self) -> None:
+        pass
+    
+    # TODO: Ajouter une nouvelle tâche à la liste avec à minima un nom et une description
+    # Utilisation de la classe TaskList pour ajouter une tâche
+    def add_task(task):
+        pass
+
+    # TODO: Marquer une tâche comme terminée.
+    # Utilisation de la classe TaskList pour marquer une tâche comme terminée
+    def complete_task(task):
+        pass
+
+    # TODO: Supprimer une tâche de la liste
+    # Utilisation de la classe TaskList pour supprimer une tâche
+    def remove_task(task):
+        pass 
+
+    # TODO: Afficher la liste des tâches en cours.
+    # Utilisation de la classe TaskList pour afficher la liste des tâches
+    def display_tasks():
+        pass
---
+++ Task_class.py
@@ -0,0 +1,7 @@
+import random 
+class Task():
+    # TODO: Create Task 
+    # Create ID for each task 
+    def __init__(self, nom, description, creation_date, state, category) -> None:
+        
+        pass
