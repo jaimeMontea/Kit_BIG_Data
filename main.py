60b54d49c0b04711e6df603fd84b156dc379ff1f
Maxime <145595205+Dustadd@users.noreply.github.com>
2023-10-10 23:29:24 +0200

    Initial commit for main.py

--- main.py
+++ main.py
@@ -1,42 +1,74 @@
-# main.py
-from task_manager import TaskManager
+from task_manager import TaskManager  # Assuming task_manager.py contains a TaskManager class
+from task import Task  # Assuming task.py contains a Task class
+
+def main():
+    task_manager = TaskManager()  # Initialize the TaskManager
+    
+    while True:
+        print("\n--- Task Manager ---")
+        print("Choose an option:")
+        print("1: Add Task")
+        print("2: Remove Task")
+        print("3: Display All Tasks")
+        print("4: Complete Task")
+        print("5: Modify Task")
+        print("6: Reset Task List")
+        print("7: Send Notification (Email)")
+        print("8: Exit")
+        
+        choice = input("Your choice: ")
+
+        if choice == '1':
+            # Add a new task
+            task_name = input("Enter task name: ")
+            task_desc = input("Enter task description: ")
+            description = input("Enter additional details for the task: ")
+            due_date = input("Enter due date (leave blank for none): ")
+            assignee = input("Enter assignee (leave blank for none): ")
+            
+            new_task = Task(task_name, task_desc, description, due_date, assignee)  # Create new Task object
+            task_manager.add_task(new_task)  # Add task to TaskManager
+            print(f"Task '{task_name}' added successfully.")
+
+        elif choice == '2':
+            # Remove a task
+            task_id = int(input("Enter the task ID to remove: "))
+            task_manager.remove_task(task_id)
+            print(f"Task with ID {task_id} removed.")
+
+        elif choice == '3':
+            # Display all tasks
+            tasks = task_manager.get_all_tasks()
+            print("\n-- List of Tasks --")
+            for task in tasks:
+                print(task)
+
+        elif choice == '4':
+            # Mark a task as completed
+            task_id = int(input("Enter the task ID to mark as completed: "))
+            task_manager.complete_task(task_id)
+            print(f"Task with ID {task_id} marked as complete.")
+
+        elif choice == '5':
+            # Modify a task (this is a placeholder, replace with your own logic)
+            print("Feature to modify task is under development.")
+
+        elif choice == '6':
+            # Reset all tasks
+            task_manager.reset_tasks()
+            print("All tasks have been reset.")
+
+        elif choice == '7':
+            # Send an email notification (this is a placeholder, replace with your own logic)
+            print("Feature to send email is under development.")
+
+        elif choice == '8':
+            # Exit the application
+            print("Exiting the Task Manager.")
+            break
+
+        else:
+            print("Invalid option. Please try again.")
 
 if __name__ == "__main__":
-    task_manager = TaskManager()
-
-    while True:
-        print("\n1: Add Task")
-        print("2: Mark Task Done")
-        print("3: Delete Task")
-        print("4: Display Tasks")
-        print("5: Quit")
-
-        choice = input("Select an option: ")
-
-        try:
-            if choice == '1':
-                name = input("Enter task name: ")
-                description = input("Enter task description: ")
-                category = input("Enter task category (optional, 'General' by default): ")
-                task_manager.add_task(name, description, category)
-
-            elif choice == '2':
-                task_id = input("Enter task ID to mark as done: ")
-                if not task_manager.mark_task_done_by_id(task_id):
-                    print("Task ID not found.")
-
-            elif choice == '3':
-                task_id = input("Enter task ID to delete: ")
-                task_manager.delete_task_by_id(task_id)
-
-            elif choice == '4':
-                print("Task List:")
-                task_manager.display_tasks()
-
-            elif choice == '5':
-                break
-            else:
-                print("Invalid option, please try again.")
-                
-        except Exception as e:
-            print(f"An error occurred: {e}")
+    main()
