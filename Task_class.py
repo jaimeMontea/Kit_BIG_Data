6a215141d8abfec14559ed29422a3003f5dae8c6
jaimeMontea <85117934+jaimeMontea@users.noreply.github.com>
2023-10-13 14:57:21 +0200

    Delete Task_class.py

--- Task_class.py
+++
@@ -1,9 +0,0 @@
-import random 
-class Task():
-    # TODO: Create Task 
-    # Create ID for each task 
-    def __init__(self, nom, description, creation_date, state, category) -> None:
-        
-        pass
-
-    #  Comment
