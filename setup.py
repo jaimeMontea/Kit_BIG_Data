45327749b600a276c18db714eb54013bd91420db
Florent BRIAND florent.briand56@gmail.com <florent.briand56@gmail.com>
2023-10-20 11:00:34 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Kit_Big_Data_To_Do_List",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # liste des dépendances:
+        "numpy>=1.18.0",
+    ],
+    author="Florent BRIAND, Maxime LEDIEU Jaime M, Edouad DUCLOY, Pierre ",
+    author_email="florent.briand56@gmail.com etc..",
+    description="To Do List helps you manage your agenda by creating a list of your tasks",
+    # L'URL de votre projet GitHub ou autre
+    url="https://github.com/jaimeMontea/Kit_BIG_Data",
+)
