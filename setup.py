411cc6b46cb5f42f08563a18d1804f191244f100
Florent BRIAND florent.briand56@gmail.com <florent.briand56@gmail.com>
2023-10-25 11:29:10 +0200

    ajout Streamlit

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="Kit_Big_Data_To_Do_List",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     install_requires=[
         # liste des dépendances:
         "numpy>=1.18.0",
     ],
     author="Florent BRIAND, Maxime LEDIEU Jaime MONTEA, Edouad DUCLOY, Pierre BILLAUD",
-    author_email="florent.briand56@gmail.com etc..",
+    author_email="florent.briand56@gmail.com",
     description="To Do List helps you manage your agenda by creating a list of your tasks",
     url="https://github.com/jaimeMontea/Kit_BIG_Data",
 )
